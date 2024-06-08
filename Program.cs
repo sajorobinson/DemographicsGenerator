c85dec346eba3b47e8d0e7a96c283f3d8405a44c
Sam Robinson <sajorobinson@gmail.com>
2024-06-08 10:12:36 -0400

    Modified testing code

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿namespace Program
+﻿using Models;
+
+namespace Program
 {
     public class Program
     {
         public static void Main()
         {
-            Console.WriteLine(Models.Town.Get().Name);
-            Console.WriteLine(Models.Town.Get().State);
+            Town town = Models.Town.Get();
+            Console.WriteLine(town.Name);
+            Console.WriteLine(town.State);
             Console.WriteLine(Models.Gender.Get().Name);
             Console.WriteLine(Models.Occupation.Get().Name);
         }
