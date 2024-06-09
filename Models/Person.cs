93b0bfc1ab79a71bb15bff10936f687190376eef
Sam Robinson <sajorobinson@gmail.com>
2024-06-09 10:49:13 -0400

    Updated to Person class

--- Models/Person.cs
+++ Models/Person.cs
@@ -4,14 +4,14 @@ namespace Models
     {
         public Name? Name { get; set; }
         public DateOnly Birthday { get; set; }
-        public Ethnicity? Ethnicity { get; set; }
         public Gender? Gender { get; set; }
         public SexualOrientation? SexualOrientation { get; set; }
-        public Address? Address { get; set; }
-        public Occupation? Occupation { get; set; }
-        public PoliticalOrientation? PoliticalOrientation { get; set; }
-        public Religion? Religion { get; set; }
+        public Ethnicity? Ethnicity { get; set; }
         public EducationalAttainment? EducationalAttainment { get; set; }
+        public Occupation? Occupation { get; set; }
         public HealthStatus? HealthStatus { get; set; }
+        public Religion? Religion { get; set; }
+        public PoliticalOrientation? PoliticalOrientation { get; set; }
+        public Address? Address { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,17 +6,50 @@ namespace Program
     {
         public static void Main()
         {
-            Town town = Models.Town.Get();
-            Console.WriteLine(town.Name);
-            Console.WriteLine(town.State);
-            Console.WriteLine(Models.Gender.Get().Name);
-            Console.WriteLine(Models.Occupation.Get().Name);
-            PoliticalOrientation politicalOrientation = Models.PoliticalOrientation.GetPoliticalOrientation();
-            politicalOrientation.Name = Models.PoliticalOrientation.GetPoliticalOrientationName(politicalOrientation);
-            Console.WriteLine(politicalOrientation.SpectrumX.ToString());
-            Console.WriteLine(politicalOrientation.SpectrumY.ToString());
-            Console.WriteLine(politicalOrientation.Name);
+            Person person = new Person
+            {
+                Name = new Name(),
+                Birthday = new DateOnly(1985, 10, 11),
+                Gender = new Gender(),
+                SexualOrientation = new SexualOrientation(),
+                Ethnicity = new Ethnicity(),
+                EducationalAttainment = new EducationalAttainment(),
+                Occupation = new Occupation(),
+                HealthStatus = new HealthStatus(),
+                Religion = new Religion(),
+                PoliticalOrientation = new PoliticalOrientation(),
+                Address = new Address()
+            };
+
+            person.Name.First = Models.Name.GetFirst();
+            person.Name.Middle = Models.Name.GetMiddle();
+            person.Name.Last = Models.Name.GetLast();
+
+            person.Gender = Models.Gender.Get();
+            person.SexualOrientation = Models.SexualOrientation.Get();
+
+            person.Ethnicity = Models.Ethnicity.Get();
+
+            person.EducationalAttainment = Models.EducationalAttainment.Get();
+            person.Occupation = Models.Occupation.Get();
+            person.HealthStatus = Models.HealthStatus.Get();
+
+            person.Religion = Models.Religion.Get();
+
+            person.PoliticalOrientation = Models.PoliticalOrientation.GetPoliticalOrientation();
+            person.PoliticalOrientation.Name = Models.PoliticalOrientation.GetPoliticalOrientationName(person.PoliticalOrientation);
             
+            Console.WriteLine(person.Name.First);
+            Console.WriteLine(person.Name.Middle);
+            Console.WriteLine(person.Name.Last);
+            Console.WriteLine(person.Gender.Name);
+            Console.WriteLine(person.SexualOrientation.Name);
+            Console.WriteLine(person.Ethnicity.Name);
+            Console.WriteLine(person.EducationalAttainment.Name);
+            Console.WriteLine(person.Occupation.Name);
+            Console.WriteLine(person.HealthStatus.Name);
+            Console.WriteLine(person.Religion.Name);
+
         }
     }
 }
