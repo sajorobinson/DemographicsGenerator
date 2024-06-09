98b097e1144ce4bcb9e3054682843c5311d09607
Sam Robinson <sajorobinson@gmail.com>
2024-06-09 08:38:31 -0400

    Fixed missing access modifiers

--- Models/Religion.cs
+++ Models/Religion.cs
@@ -2,7 +2,7 @@ namespace Models
 {
     public class Religion
     {
-        string? Name { get; set; }
+        public string? Name { get; set; }
         public static Religion Get()
         {
             Religion religion = Helpers.Utility.GetResource<Religion>(Constants.Resources.ReligionFilePath);
--- Models/SexualOrientation.cs
+++ Models/SexualOrientation.cs
@@ -2,7 +2,7 @@ namespace Models
 {
     public class SexualOrientation
     {
-        string? Name {get; set;}
+        public string? Name {get; set;}
         public static SexualOrientation Get()
         {
             SexualOrientation sexualOrientation = Helpers.Utility.GetResource<SexualOrientation>(Constants.Resources.SexualOrientationFilePath);
