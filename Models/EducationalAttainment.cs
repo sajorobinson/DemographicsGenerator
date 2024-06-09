5969a377abf3e1f5e160748d084d81fa9dd25760
Sam Robinson <sajorobinson@gmail.com>
2024-06-09 10:48:24 -0400

    Added accessor modifier

--- Models/EducationalAttainment.cs
+++ Models/EducationalAttainment.cs
@@ -2,7 +2,7 @@ namespace Models
 {
     public class EducationalAttainment
     {
-        string? Name { get; set; }
+        public string? Name { get; set; }
         public static EducationalAttainment Get()
         {
             EducationalAttainment educationalAttainment = Helpers.Utility.GetResource<EducationalAttainment>(Constants.Resources.EducationalAttainmentFilePath);
