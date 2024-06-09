afa4b017f6dbdd765f68dd8eb47bd133528d5315
Sam Robinson <sajorobinson@gmail.com>
2024-06-09 08:35:02 -0400

    Added placeholder methods for generating name parameters.

--- Models/Name.cs
+++ Models/Name.cs
@@ -5,5 +5,17 @@ namespace Models
         public string? First { get; set; }
         public string? Middle { get; set; }
         public string? Last { get; set; }
+        public static string GetFirst()
+        {
+            return "PlaceholderFirst";
+        }
+        public static string GetMiddle()
+        {
+            return "PlaceholderMiddle";
+        }
+        public static string GetLast()
+        {
+            return "PlaceholderLast";
+        }
     }
 }
