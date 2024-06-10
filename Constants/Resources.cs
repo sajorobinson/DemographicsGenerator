a23648b58bf24c63cb9398e58ef49c95e0c613b3
Sam Robinson <sajorobinson@gmail.com>
2024-06-10 07:49:21 -0400

    Modifying file path constants

--- Constants/Resources.cs
+++ Constants/Resources.cs
@@ -3,11 +3,12 @@ namespace Constants
     public static class Resources
     {
         public static readonly string BaseFilePath = "./Resources/";
-        public static readonly string CountryFilePath = BaseFilePath + "Country.json";
         public static readonly string EducationalAttainmentFilePath = BaseFilePath + "EducationalAttainment.json";
         public static readonly string EthnicityFilePath = BaseFilePath + "Ethnicity.json";
         public static readonly string GenderFilePath = BaseFilePath + "Gender.json";
         public static readonly string HealthStatusFilePath = BaseFilePath + "HealthStatus.json";
+        public static readonly string HobbyFilePath = BaseFilePath + "Hobby.json";
+        public static readonly string LanguageFilePath = BaseFilePath + "Language.json";
         public static readonly string OccupationFilePath = BaseFilePath + "Occupation.json";
         public static readonly string ReligionFilePath = BaseFilePath + "Religion.json";
         public static readonly string SexualOrientationFilePath = BaseFilePath + "SexualOrientation.json";
