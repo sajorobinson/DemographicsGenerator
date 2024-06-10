68b0d8eaf94fc9c732a42ae8a7d2d7d31eacb11e
Sam Robinson <sajorobinson@gmail.com>
2024-06-10 07:49:29 -0400

    Testing code changes

--- Program.cs
+++ Program.cs
@@ -15,41 +15,16 @@ namespace Program
                 Ethnicity = new Ethnicity(),
                 EducationalAttainment = new EducationalAttainment(),
                 Occupation = new Occupation(),
+                Hobby = new Hobby(),
                 HealthStatus = new HealthStatus(),
                 Religion = new Religion(),
                 PoliticalOrientation = new PoliticalOrientation(),
                 Address = new Address()
             };
 
-            person.Name.First = Models.Name.GetFirst();
-            person.Name.Middle = Models.Name.GetMiddle();
-            person.Name.Last = Models.Name.GetLast();
-
-            person.Gender = Models.Gender.Get();
-            person.SexualOrientation = Models.SexualOrientation.Get();
-
-            person.Ethnicity = Models.Ethnicity.Get();
-
-            person.EducationalAttainment = Models.EducationalAttainment.Get();
-            person.Occupation = Models.Occupation.Get();
-            person.HealthStatus = Models.HealthStatus.Get();
-
-            person.Religion = Models.Religion.Get();
-
-            person.PoliticalOrientation = Models.PoliticalOrientation.GetPoliticalOrientation();
-            person.PoliticalOrientation.Name = Models.PoliticalOrientation.GetPoliticalOrientationName(person.PoliticalOrientation);
-            
-            Console.WriteLine(person.Name.First);
-            Console.WriteLine(person.Name.Middle);
-            Console.WriteLine(person.Name.Last);
-            Console.WriteLine(person.Gender.Name);
-            Console.WriteLine(person.SexualOrientation.Name);
-            Console.WriteLine(person.Ethnicity.Name);
-            Console.WriteLine(person.EducationalAttainment.Name);
-            Console.WriteLine(person.Occupation.Name);
-            Console.WriteLine(person.HealthStatus.Name);
-            Console.WriteLine(person.Religion.Name);
-
+            Console.WriteLine(person.SexualOrientation.GetRandomName());
+            Console.WriteLine(person.Hobby.GetRandomName());
+            Console.WriteLine(person.Address.Street);
         }
     }
 }
