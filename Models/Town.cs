25ae6e54bb58039afaccbe48d4d1c631f896eb34
Sam Robinson <sajorobinson@gmail.com>
2024-06-10 07:48:23 -0400

    Changing method from static to not

--- Models/Town.cs
+++ Models/Town.cs
@@ -6,10 +6,10 @@ namespace Models
         public string? State { get; set; }
         public string? Country { get; set; }
         public List<Person>? Population { get; set; }
-        public static Town Get()
+        public string? GetRandomName()
         {
             Town town = Helpers.Utility.GetResource<Town>(Constants.Resources.TownFilePath);
-            return town;
+            return town.Name;
         }
     }
 }
