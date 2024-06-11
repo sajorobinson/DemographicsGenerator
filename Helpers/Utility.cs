14ea452d761ffe2039483d29dd8a37ee33b9d309
Sam Robinson <sajorobinson@gmail.com>
2024-06-11 15:07:11 -0400

    Made GetNewRandom private

--- Helpers/Utility.cs
+++ Helpers/Utility.cs
@@ -1,24 +1,28 @@
+using System.Reflection;
 using System.Text.Json;
 
 namespace DemographicsGenerator.Helpers
 {
     public static class Utility
     {
-        public static Random GetNewRandom()
+        private static Random GetNewRandom()
         {
             Random random = new Random();
             return random;
         }
+
         public static T GetRandomItem<T>(List<T> list)
         {
             int randomIndex = GetNewRandom().Next(list.Count);
             return list[randomIndex];
         }
+
         public static int GetRandomNumber(int x, int y)
         {
             int randomNumber = GetNewRandom().Next(x, y + 1);
             return randomNumber;
         }
+
         public static T GetResource<T>(string filePath)
         {
             string json = Json.GetJsonString(filePath);
