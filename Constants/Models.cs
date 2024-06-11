a9274008875291881bcea5ef8de94e53ffad476f
Sam Robinson <sajorobinson@gmail.com>
2024-06-11 15:06:39 -0400

    Changed DefaultName to be more descriptive for testing

--- Constants/Models.cs
+++ Constants/Models.cs
@@ -2,6 +2,6 @@ namespace DemographicsGenerator.Constants
 {
     public static class Models
     {
-        public static readonly string DefaultName = "Prefer not to say";
+        public static readonly string DefaultName = "Default response";
     }
 }
