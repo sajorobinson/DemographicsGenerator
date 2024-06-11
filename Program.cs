739e8bdd94bcaac09a03b15731212398881911b9
Sam Robinson <sajorobinson@gmail.com>
2024-06-11 15:08:49 -0400

    Testing code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using DemographicsGenerator.Models;
+﻿using DemographicsGenerator.Helpers;
+using DemographicsGenerator.Models;
 
 namespace DemographicsGenerator
 {
@@ -6,14 +7,46 @@ namespace DemographicsGenerator
     {
         public static void Main()
         {
-            Town town = new Town();
-            Town randomTown = Models.Town.GetRandomTown();
-            town.Name = randomTown.Name;
-            town.State = randomTown.State;
-            town.Country = randomTown.Country;
-            Console.WriteLine(town.Name);
-            Console.WriteLine(town.State);
-            Console.WriteLine(town.Country);
+            Person person = new Person()
+            {
+                Name = new Name(),
+                Address = new Address(),
+                Birthday = new DateOnly(1985, 10, 11),
+                EducationalAttainment = new EducationalAttainment(),
+                Ethnicity = new Ethnicity(),
+                Gender = new Gender(),
+                HealthStatus = new HealthStatus(),
+                Hobby = new Hobby(),
+                Occupation = new Occupation(),
+                PoliticalOrientation = new PoliticalOrientation()
+            };
+            person.Name.First = Name.GetFirst();
+            person.Name.Middle = Name.GetMiddle();
+            person.Name.Last = Name.GetLast();
+            person.Address.Street = Address.GetStreet();
+            person.Address.Town = Address.GetTown();
+            person.EducationalAttainment = EducationalAttainment.GetRandomEducationalAttainment();
+            person.Ethnicity = Ethnicity.GetRandomEthnicity();
+            person.Gender = Gender.GetRandomGender();
+            person.HealthStatus = HealthStatus.GetRandomHealthStatus();
+            person.Hobby = Hobby.GetRandomHobby();
+            person.Occupation = Occupation.GetRandomOccupation();
+            person.PoliticalOrientation.Name = PoliticalOrientation.GetPoliticalOrientationName(person.PoliticalOrientation);
+            
+            Console.WriteLine(person.Name.First);
+            Console.WriteLine(person.Name.Middle);
+            Console.WriteLine(person.Name.Last);
+            Console.WriteLine(person.Address.Street);
+            Console.WriteLine(person.Address.Town.Name);
+            Console.WriteLine(person.Address.Town.State);
+            Console.WriteLine(person.Address.Town.Country);
+            Console.WriteLine(person.EducationalAttainment.Name);
+            Console.WriteLine(person.Ethnicity.Name);
+            Console.WriteLine(person.Gender.Name);
+            Console.WriteLine(person.HealthStatus.Name);
+            Console.WriteLine(person.Hobby.Name);
+            Console.WriteLine(person.Occupation.Name);
+            Console.WriteLine(person.PoliticalOrientation.Name);
         }
     }
 }
